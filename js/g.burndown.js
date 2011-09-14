6a157af0a0fc7cb2f8b213c5946ac7e9ab51c7f9
Glenn Nilsson <glenn.nilsson@gmail.com>
2011-09-14 21:37:51 +0200

    Revert to solution to build all manually. Too much hassle with the g lib. Axises drawn with nice variables. Data harvested.

--- js/g.burndown.js
+++ js/g.burndown.js
@@ -1,19 +1,4 @@
-/*!
- * g.Raphael 0.4.2 - Charting library, based on Raphaël
- *
- * Copyright (c) 2009 Dmitry Baranovskiy (http://g.raphaeljs.com)
- * Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
- */
-Raphael.fn.g.burndown = function (x, y, width, height, dates, original, added, opts) {
-  
-  
-  var Path = function() {
-    this.path = "";
-  }
-  Path.prototype.toString = function() { return this.path.substr(1); }
-  Path.prototype.M = function(x, y) { this.path += " M " + x + " " + y; return this; }
-  Path.prototype.L = function(x, y) { this.path += " L " + x + " " + y; return this; }
-  Path.prototype.Z = function() { this.path += " Z"; return this; }
+(function() {
   
   var max = function(a, f) {
     f = f || function(p, c) {
@@ -27,92 +12,172 @@ Raphael.fn.g.burndown = function (x, y, width, height, dates, original, added, o
     }
     return a.reduce(f, 0);
   }
+  var r = function(f) {
+    return Math.round(f) + .5;
+  }
+
+
+  var Path = function() {
+    this.path = "";
+  }
+  Path.prototype.toString = function() { return this.path.substr(1); }
+  Path.prototype.move = function(x, y) { this.path += " M " + r(x) + " " + r(y); return this; }
+  Path.prototype.line = function(x, y) { this.path += " L " + r(x) + " " + r(y); return this; }
+  Path.prototype.clone = function() { this.path += " Z"; return this; }
+
+
+
+
+/**
+
+data = [
+  { label: '2011-09-12', remaining: 7, added: 0 },
+  { label: '2011-09-13', remaining: 7, added: 1 },
+  { label: '2011-09-14', remaining: 5, added: 1 },
+  { label: '2011-09-15', remaining: 2, added: 0 },
+  { label: '2011-09-16', remaining: 0, added: 0 }
+]
+
+**/
+
+Raphael.fn.g.burndown = function (x, y, width, height, data) {
 
   var gutter = {
-    top: 10,
-    right: 30,
-    bottom: 80,
-    left: 30
-  };
-
-  var chart = this.set();
+        top: 10,
+        right: 30,
+        bottom: 80,
+        left: 30
+      }
+    , availWidth = width - gutter.left - gutter.right
+    , availHeight = height - gutter.top - gutter.bottom
+    , corners = {
+        top: gutter.top
+      , right: gutter.left + availWidth
+      , bottom: gutter.top + availHeight
+      , left: gutter.left
+      }
+    , points = {}
+    , colors = {
+        axis: 'black'
+      }
+    ;
   
-  var maxOriginalPoints = max(original);
-  var maxAddedPoints = max(added);
-  var maxTotalPoints = maxAddedPoints + maxOriginalPoints;
-  var availHeight = height - gutter.top - gutter.bottom;
-  var availWidth = width - gutter.left - gutter.right;
+  points.totalAdded = data.reduce(function(t, c) { return t + (c.added || 0); }, 0);
+  points.originalRemaining = data[0] ? data[0].remaining || 0 : 0;
+  points.total = points.totalAdded + points.originalRemaining;
   
-  var axisPointsMax = Math.ceil(maxOriginalPoints / 5) * 5;
-  var axisPointsMin = Math.floor(-maxAddedPoints / 5) * 5;
-  var axisPointsTotal = axisPointsMax - axisPointsMin;
+  var columnWidth = availWidth / data.length;
   
-  var heightOriginal = axisPointsMax !== 0 ? availHeight * (axisPointsMax / axisPointsTotal) : 0;
-  var heightAdded = maxAddedPoints !== 0 ? availHeight * (-axisPointsMin / axisPointsTotal) : 0;
-
-    // function (x, y, length, from, to, steps, orientation, labels, type, dashsize) {
-      // orientation: 0:hor,below, 1:ver:left, 2:hor:above, 3: ver:right
-  var steps = axisPointsTotal / 10;
-
-  this.g.axis(gutter.left, gutter.top + heightOriginal + heightAdded, heightOriginal + heightAdded, axisPointsMin, axisPointsMax, steps, 1);
-  this.g.axis(width - gutter.right, gutter.top + heightOriginal + heightAdded, heightOriginal + heightAdded, axisPointsMin, axisPointsMax, steps, 3);
-  
-  var origo = {
-    x: gutter.left,
-    y: gutter.top + heightOriginal
-  };
-  
-  this.path(new Path()
-    .M(origo.x, origo.y)
-    .L(width - gutter.right, origo.y)
-  );
-  
-  var dx = availWidth/dates.length;
-  
-  for (var i=0, d, x; (d=dates[i]) && (x=gutter.left+i*dx); i++) {
-    // Text
-    var text = this.text(x+dx/2, height-40, d);
-    text.rotate(31).attr({
-      'font-size': '12px'
-    });
-    
-    if (i === 0) continue;
-    
-    // Line
-    this.path(new Path().M(x, gutter.top).L(x, height - gutter.bottom)).attr({
-      opacity: .5
-    });
-  }
-  
-  var lines = this.set();
-  var p, x, y, c = this.g.colors[0];
-  for (var i=0, val; val=original[i]; i++) {
-    x = gutter.left + dx/2 + dx*i;
-    y = origo.y - (val/axisPointsMax) * heightOriginal;
-    
-    if (p === undefined) {
-      p = new Path().M(x, y);
-    } else {
-      p.L(x, y);
+  var d = data.map(function(d, i) {
+    return {
+      label: d.label,
+      remaining: d.remaining,
+      added: d.added,
+      coords: {
+        left: gutter.left + columnWidth * i,
+        center: gutter.left + columnWidth * (i + .5)
+      }
     }
+  });
+  
+  var chart = this.set(),
+      axis = this.set();
+  
+  // Draw chart axis
+  axis.push(this.path(new Path()
+    .move(corners.left, corners.top)
+    .line(corners.left, corners.bottom)
+  ));
+  
+  // Check if we have data for at least the first day.
+  if (points.total) {
     
-    this.g.disc(x, y, 5).attr({
-      fill: c,
-      stroke: 'none'
-    });
+    var base = gutter.top + (availHeight * (points.originalRemaining / points.total));
+    
+    // Draw base line
+    axis.push(this.path(new Path()
+      .move(corners.left, base)
+      .line(corners.right, base)
+    ));
   }
-  // Line
-  this.path(p).attr({
-    'stroke': c,
-    'stroke-width': 2
-  });
-  // Shadow
-  p.L(x, origo.y).L(gutter.left + dx/2, origo.y).Z();
-  this.path(p).attr({
-    'stroke': 'none',
-    'fill': c,
-    'opacity': .5
-  });
+  
+  
+  // var maxOriginalPoints = max(original);
+  // var maxAddedPoints = max(added);
+  // var maxTotalPoints = maxAddedPoints + maxOriginalPoints;
+  // var availHeight = height - gutter.top - gutter.bottom;
+  // var availWidth = width - gutter.left - gutter.right;
+  // 
+  // var axisPointsMax = Math.ceil(maxOriginalPoints / 5) * 5;
+  // var axisPointsMin = Math.floor(-maxAddedPoints / 5) * 5;
+  // var axisPointsTotal = axisPointsMax - axisPointsMin;
+  // 
+  // var heightOriginal = axisPointsMax !== 0 ? availHeight * (axisPointsMax / axisPointsTotal) : 0;
+  // var heightAdded = maxAddedPoints !== 0 ? availHeight * (-axisPointsMin / axisPointsTotal) : 0;
+  // 
+  //   // function (x, y, length, from, to, steps, orientation, labels, type, dashsize) {
+  //     // orientation: 0:hor,below, 1:ver:left, 2:hor:above, 3: ver:right
+  // var steps = axisPointsTotal / 10;
+  // 
+  // this.g.axis(gutter.left, gutter.top + heightOriginal + heightAdded, heightOriginal + heightAdded, axisPointsMin, axisPointsMax, steps, 1);
+  // this.g.axis(width - gutter.right, gutter.top + heightOriginal + heightAdded, heightOriginal + heightAdded, axisPointsMin, axisPointsMax, steps, 3);
+  // 
+  // var origo = {
+  //   x: gutter.left,
+  //   y: gutter.top + heightOriginal
+  // };
+  // 
+  // this.path(new Path()
+  //   .M(origo.x, origo.y)
+  //   .L(width - gutter.right, origo.y)
+  // );
+  // 
+  // var dx = availWidth/dates.length;
+  // 
+  // for (var i=0, d, x; (d=dates[i]) && (x=gutter.left+i*dx); i++) {
+  //   // Text
+  //   var text = this.text(x+dx/2, height-40, d);
+  //   text.rotate(31).attr({
+  //     'font-size': '12px'
+  //   });
+  //   
+  //   if (i === 0) continue;
+  //   
+  //   // Line
+  //   this.path(new Path().M(x, gutter.top).L(x, height - gutter.bottom)).attr({
+  //     opacity: .5
+  //   });
+  // }
+  // 
+  // var lines = this.set();
+  // var p, x, y, c = this.g.colors[0];
+  // for (var i=0, val; val=original[i]; i++) {
+  //   x = gutter.left + dx/2 + dx*i;
+  //   y = origo.y - (val/axisPointsMax) * heightOriginal;
+  //   
+  //   if (p === undefined) {
+  //     p = new Path().M(x, y);
+  //   } else {
+  //     p.L(x, y);
+  //   }
+  //   
+  //   this.g.disc(x, y, 5).attr({
+  //     fill: c,
+  //     stroke: 'none'
+  //   });
+  // }
+  // // Line
+  // this.path(p).attr({
+  //   'stroke': c,
+  //   'stroke-width': 2
+  // });
+  // // Shadow
+  // p.L(x, origo.y).L(gutter.left + dx/2, origo.y).Z();
+  // this.path(p).attr({
+  //   'stroke': 'none',
+  //   'fill': c,
+  //   'opacity': .5
+  // });
   
     
 return chart;
@@ -167,3 +232,6 @@ return chart;
     };
     return chart;
 };
+
+})();
+
--- test2.js
+++ test2.js
@@ -1,52 +1,12 @@
-var data = {
-  items: [
-    { id: "1", title: "Test 1" },
-    { id: "2", title: "Test 2" },
-    { id: "3", title: "Test 3" },
-    { id: "4", title: "Test 4" },
-    { id: "5", title: "Test 5" },
-    { id: "6", title: "Test 6" }
-  ],
-  nodes: [
-    { date: '2011-09-09', data: [ { id: "1", v: 5 }, { id: "2", v: 2 }, { id: "3", v: 5 }, { id: "4", v: 5 }, { id: "5", v: 2 }, { id: "6", v: 3 } ] },
-    { date: '2011-09-10', data: [ { id: "1", v: 4 }, { id: "2", v: 1 }, { id: "3", v: 5 }, { id: "4", v: 4 }, { id: "5", v: 4 }, { id: "6", v: 3 } ] },
-    { date: '2011-09-11', data: [ { id: "1", v: 1 }, { id: "2", v: 0 }, { id: "3", v: 5 }, { id: "4", v: 4 }, { id: "5", v: 2 }, { id: "6", v: 3 }, { id: "7", v: 2 } ] },
-    { date: '2011-09-12', data: [ { id: "1", v: 0 }, { id: "2", v: 0 }, { id: "3", v: 2 }, { id: "4", v: 1 }, { id: "5", v: 0 }, { id: "6", v: 2 }, { id: "7", v: 2 } ] },
-    { date: '2011-09-13', data: [ { id: "1", v: 0 }, { id: "2", v: 0 }, { id: "3", v: 0 }, { id: "4", v: 1 }, { id: "5", v: 0 }, { id: "6", v: 0 }, { id: "7", v: 0 } ] }
-  ]
-}
-
-var max = function(a, f) {
-  f = f || function(p, c) {
-    return p < c ? c : p;
-  }
-  return a.reduce(f, 0);
-}
-var total = function(a, f) {
-  f = f || function(p, c) {
-    return p + c;
-  }
-  return a.reduce(f, 0);
-}
+var data = [
+  { label: '2011-09-12', remaining: 7, added: 0 },
+  { label: '2011-09-13', remaining: 7, added: 1 },
+  { label: '2011-09-14', remaining: 5, added: 1 },
+  { label: '2011-09-15', remaining: 2, added: 0 },
+  { label: '2011-09-16', remaining: 0, added: 0 }
+]
 
 window.addEventListener('load', function () {
   var r = Raphael("sprint");
-  
-  var dates = [];
-  var remaining = [];
-  var added = [];
-  var countItemsStart = data.nodes[0].data.length;
-  data.nodes.forEach(function(d, i) {
-    dates.push(d.date);
-    remaining.push(total(d.data.slice(0, countItemsStart), function(p, c) { return p + c.v; }));
-    added.push(total(d.data.slice(countItemsStart), function(p, c) { return p + c.v; }));
-  });
-  
-  // temp real data
-  dates = "2011-09-13 2011-09-14 2011-09-15 2011-09-18 2011-09-19 2011-09-20 2011-09-21 2011-09-22".split(' ');
-  remaining = [146, 144];
-  added = [0, 5];
-  
-  r.g.burndown(0, 0, 500, 400, dates, remaining, added, {});
-
+  r.g.burndown(0, 0, 500, 400, data, {});
 });
