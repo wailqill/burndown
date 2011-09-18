a205415e28b128dc722c73fff9ef9e06b0bcba6d
Glenn Nilsson <glenn.nilsson@gmail.com>
2011-09-18 18:24:32 +0200

    Do most calculations up-front, and restructure the code.

--- js/g.burndown.js
+++ js/g.burndown.js
@@ -34,250 +34,211 @@
   Path.prototype.line = function(x, y) { this.path += " L " + r(x) + " " + r(y); return this; }
   Path.prototype.close = function() { this.path += " Z"; return this; }
 
-Raphael.fn.g.burndown = function (x, y, width, height, data) {
+  Raphael.fn.g.burndown = function (x, y, width, height, data) {
 
-  var gutter = {
-        top: 10,
-        right: 30,
-        bottom: 80,
-        left: 30
+    var gutter = {
+          top: 10,
+          right: 30,
+          bottom: 80,
+          left: 30
+        }
+      , availWidth = width - gutter.left - gutter.right
+      , availHeight = height - gutter.top - gutter.bottom
+      , corners = {
+          top: gutter.top
+        , right: gutter.left + availWidth
+        , bottom: gutter.top + availHeight
+        , left: gutter.left
+        }
+      , columnWidth = availWidth / data.length
+      , points = {}
+      , axis = {}
+      , colors = {
+          shadowOpacity: .3,
+          axis: 'hsl(0, 0, 40%)',
+          remaining: 'blue',
+          added: 'red',
+          optimal: 'green',
+          actual: 'purple',
+          columnHelper: 'rgba(0, 0, 0, .1)'
+        }
+      ;
+  
+    var accAdded = 0;
+    data = data.map(function(d, i) {
+      accAdded += d.added || 0;
+      var o = {
+        label: d.label,
+        accAdded: accAdded,
+        coords: {
+          left: gutter.left + columnWidth * i,
+          center: gutter.left + columnWidth * (i + .5),
+          right: gutter.left + columnWidth * (i + 1)
+        }
       }
-    , availWidth = width - gutter.left - gutter.right
-    , availHeight = height - gutter.top - gutter.bottom
-    , corners = {
-        top: gutter.top
-      , right: gutter.left + availWidth
-      , bottom: gutter.top + availHeight
-      , left: gutter.left
+      if (d.remaining !== undefined) {
+        o.remaining = d.remaining;
+        o.baseRemaining = d.remaining - accAdded;
       }
-    , columnWidth = availWidth / data.length
-    , points = {}
-    , axis = {}
-    , colors = {
-        shadowOpacity: .3,
-        axis: 'hsl(0, 0, 40%)',
-        remaining: 'blue',
-        added: 'red',
-        optimal: 'green',
-        actual: 'purple',
-        columnHelper: 'rgba(0, 0, 0, .1)'
+      if (d.added !== undefined) {
+        o.added = d.added;
       }
-    ;
-  
-  var accAdded = 0;
-  data = data.map(function(d, i) {
-    accAdded += d.added || 0;
-    var o = {
-      label: d.label,
-      accAdded: accAdded,
-      coords: {
-        left: gutter.left + columnWidth * i,
-        center: gutter.left + columnWidth * (i + .5)
-      }
-    }
-    if (d.remaining !== undefined) {
-      o.remaining = d.remaining;
-      o.baseRemaining = d.remaining - accAdded;
-    }
-    if (d.added !== undefined) {
-      o.added = d.added;
-    }
     
-    return o;
-  });
-
-  points.totalAdded             = data.reduce(function(t, c) { return t + (c.added || 0); }, 0);
-  points.originalRemaining      = data[0] ? data[0].remaining || 0 : 0;
-  points.uppermostBaseRemaining = data.reduce(function(t, c) { return Math.max(t, c.baseRemaining || 0); }, 0);
-  points.total                  = points.totalAdded + points.uppermostBaseRemaining;
-
-  // If no data. exit.
-  if (points.total === 0)
-    return;
-
-  axis.deltaPoints              = calculatePointDelta(points.total);
-  axis.upPoints                 = Math.ceil(points.uppermostBaseRemaining / axis.deltaPoints) * axis.deltaPoints;
-  axis.downPoints               = Math.ceil(points.totalAdded / axis.deltaPoints) * axis.deltaPoints;
-  axis.totalPoints              = axis.upPoints + axis.downPoints;
-  axis.deltaY                   = availHeight / (axis.upPoints + axis.downPoints);
-
-  var baseOriginal  = gutter.top + (availHeight * (axis.upPoints / axis.totalPoints));
-  var baseAdded     = baseOriginal + accAdded * axis.deltaY;
-  
-  var sets = {
-    axis: this.set(),
-  };
-  
-  // Draw point axis line
-  sets.axis.push(this.path(new Path()
-    .move(corners.left, corners.top)
-    .line(corners.left, corners.bottom)
-  ).attr('stroke', colors.axis));
-  
-  // Draw point axis markings
-  var point = -axis.downPoints;
-  while (point <= axis.upPoints) {
-    // Calc y pos
-    var y = baseOriginal - point*axis.deltaY;
+      o.hasData = o.remaining !== undefined;
     
-    // Draw line
-    sets.axis.push(this.path(new Path()
-      .move(corners.left - 2, y)
-      .line(corners.left + 2, y)
-    ).attr('stroke', colors.axis));
-    
-    // Draw text
-    sets.axis.push(this.text(gutter.left / 2, y, Math.abs(point)));
-    
-    point += axis.deltaPoints;
-  }
-    
-  // Draw base line
-  sets.axis.push(this.path(new Path()
-    .move(corners.left, baseOriginal)
-    .line(corners.right, baseOriginal)
-  ).attr({
-    'stroke': colors.axis,
-    'opacity': .5 
-  }));
-
-  // Draw accAdded base line
-  sets.axis.push(this.path(new Path()
-    .move(corners.left, baseAdded)
-    .line(corners.right, baseAdded)
-  ).attr('stroke', colors.axis));
-  
-  var above = { path: new Path() },
-      below = { path: new Path() };
-  
-  for (var i=0,d; d=data[i]; i++) {
-    var x = gutter.left + (i + .5) * columnWidth;
-    
-    // Draw column helper lines
-    this.path(new Path()
-      .move(x + .5 * columnWidth, corners.top)
-      .line(x + .5 * columnWidth, corners.bottom)
-    ).attr({
-      'stroke': colors.columnHelper
+      return o;
     });
-    
-    // Draw dates as labels
-    this.text(x, corners.bottom + gutter.bottom/2, d.label).rotate(300);
 
-    if (d.baseRemaining !== undefined) {
+    points.totalAdded             = data.reduce(function(t, c) { return t + (c.added || 0); }, 0);
+    points.originalRemaining      = data[0] ? data[0].remaining || 0 : 0;
+    points.uppermostBaseRemaining = data.reduce(function(t, c) { return Math.max(t, c.baseRemaining || 0); }, 0);
+    points.total                  = points.totalAdded + points.uppermostBaseRemaining;
 
-      above.x = below.x = x;
-      
-      // Above
-      above.y = baseOriginal - d.baseRemaining * axis.deltaY;
-      var f = i > 0 ? above.path.line : above.path.move;
-      f.call(above.path, above.x, above.y);
+    // If no data. exit.
+    if (!data.some(function(d) { return d.hasData; }))
+      return;
+
+    axis.deltaPoints              = calculatePointDelta(points.total);
+    axis.upPoints                 = Math.ceil(points.uppermostBaseRemaining / axis.deltaPoints) * axis.deltaPoints;
+    axis.downPoints               = Math.ceil(points.totalAdded / axis.deltaPoints) * axis.deltaPoints;
+    axis.totalPoints              = axis.upPoints + axis.downPoints;
+    axis.deltaY                   = availHeight / (axis.upPoints + axis.downPoints);
+
+    var baseOriginal  = gutter.top + (availHeight * (axis.upPoints / axis.totalPoints));
+    var baseAdded     = baseOriginal + accAdded * axis.deltaY;
+  
+    data = data.map(function(d, i) {
+      d.coords.top = baseOriginal - d.baseRemaining*axis.deltaY;
+      d.coords.bottom = baseOriginal + d.accAdded*axis.deltaY;
     
-      // Dots
-      this.circle(above.x, above.y, 4).attr({
+      return d;
+    });
+  
+    var sets = {
+      axis: this.set(),
+    };
+  
+    // Draw point axis line
+    sets.axis.push(this.path(new Path()
+      .move(corners.left, corners.top)
+      .line(corners.left, corners.bottom)
+    ).attr('stroke', colors.axis));
+  
+    // Draw point axis markings
+    var point = -axis.downPoints;
+    while (point <= axis.upPoints) {
+      // Calc y pos
+      var y = baseOriginal - point*axis.deltaY;
+    
+      // Draw line
+      sets.axis.push(this.path(new Path()
+        .move(corners.left - 2, y)
+        .line(corners.left + 2, y)
+      ).attr('stroke', colors.axis));
+    
+      // Draw text
+      sets.axis.push(this.text(gutter.left / 2, y, Math.abs(point)));
+    
+      point += axis.deltaPoints;
+    }
+    
+    // Draw base line
+    sets.axis.push(this.path(new Path()
+      .move(corners.left, baseOriginal)
+      .line(corners.right, baseOriginal)
+    ).attr({
+      'stroke': colors.axis,
+      'opacity': .5
+    }));
+
+    // Draw accAdded base line
+    sets.axis.push(this.path(new Path()
+      .move(corners.left, baseAdded)
+      .line(corners.right, baseAdded)
+    ).attr('stroke', colors.axis));
+  
+  
+    var paths = {
+      top: new Path(),
+      bottom: new Path(),
+      topShadow: new Path(),
+      bottomShadow: new Path()
+    };
+    for (var i=0; d=data[i]; i++) {
+    
+      // Draw column helper lines
+      this.path(new Path()
+        .move(d.coords.right, corners.top)
+        .line(d.coords.right, corners.bottom)
+      ).attr({
+        'stroke': colors.columnHelper
+      });
+    
+      // Draw dates as labels
+      this.text(d.coords.center, corners.bottom + gutter.bottom/2, d.label).rotate(300);
+    
+      if (!d.hasData) continue;
+    
+      // Dot remaining
+      this.circle(d.coords.center, d.coords.top, 4).attr({
         'fill': colors.remaining,
         'stroke': 'none'
       });
-  
-      // Below
-      below.y = baseOriginal + d.accAdded * axis.deltaY;
-      var f = i > 0 ? below.path.line : below.path.move;
-      f.call(below.path, below.x, below.y);
-      
-      // Dots
-      this.circle(below.x, below.y, 4).attr({
+    
+      // Dot added
+      this.circle(d.coords.center, d.coords.bottom, 4).attr({
         'fill': colors.added,
         'stroke': 'none'
       });
-    }
-  }
-  
-  // Draw optimal burn down line
-  this.path(new Path()
-    .move(corners.left + .5 * columnWidth, baseOriginal - data[0].baseRemaining * axis.deltaY)
-    .line(corners.right - .5 * columnWidth, baseOriginal + accAdded * axis.deltaY)
-  ).attr({
-    'stroke': colors.optimal
-  }).toBack();
-
-  this.path(above.path).attr({
-    'stroke': colors.remaining,
-    'stroke-width': '2px'
-  });
-  above.path.line(above.x, baseOriginal).line(corners.left + .5 * columnWidth, baseOriginal).close();
-  this.path(above.path).attr({
-    'fill': colors.remaining,
-    'stroke': 'none',
-    'opacity': colors.shadowOpacity
-  }).toBack();
-  
-  this.path(below.path).attr({
-    'stroke': colors.added,
-    'stroke-width': '2px'
-  });
-  below.path.line(below.x, baseOriginal).line(corners.left + .5 * columnWidth, baseOriginal).close();
-  this.path(below.path).attr({
-    'fill': colors.added,
-    'stroke': 'none',
-    'opacity': colors.shadowOpacity
-  }).toBack();
-  
-
-  
-  
-  
     
-return this.set(sets.axis);
+      // Find path function to use
+      var f = i === 0 ? Path.prototype.move : Path.prototype.line;
+    
+      // Set line paths
+      f.call(paths.top, d.coords.center, d.coords.top);
+      f.call(paths.bottom, d.coords.center, d.coords.bottom);
+    
+      // Special treatment for shadows
+      if (i === 0) {
+        paths.topShadow.move(d.coords.center, baseOriginal);
+        paths.bottomShadow.move(d.coords.center, baseOriginal);
+      }
+      paths.topShadow.line(d.coords.center, d.coords.top);
+      paths.bottomShadow.line(d.coords.center, d.coords.bottom);
+    }
+    
+    var lastWithData = data.filter(function(d){ return d.hasData; }).pop();
+    paths.topShadow.line(lastWithData.coords.center, baseOriginal).close();
+    paths.bottomShadow.line(lastWithData.coords.center, baseOriginal).close();
 
-    chart.push(lines, shades, symbols, axis, columns, dots);
-    chart.lines = lines;
-    chart.shades = shades;
-    chart.symbols = symbols;
-    chart.axis = axis;
-    chart.hoverColumn = function (fin, fout) {
-        !columns && createColumns();
-        columns.mouseover(fin).mouseout(fout);
-        return this;
-    };
-    chart.clickColumn = function (f) {
-        !columns && createColumns();
-        columns.click(f);
-        return this;
-    };
-    chart.hrefColumn = function (cols) {
-        var hrefs = that.raphael.is(arguments[0], "array") ? arguments[0] : arguments;
-        if (!(arguments.length - 1) && typeof cols == "object") {
-            for (var x in cols) {
-                for (var i = 0, ii = columns.length; i < ii; i++) if (columns[i].axis == x) {
-                    columns[i].attr("href", cols[x]);
-                }
-            }
-        }
-        !columns && createColumns();
-        for (i = 0, ii = hrefs.length; i < ii; i++) {
-            columns[i] && columns[i].attr("href", hrefs[i]);
-        }
-        return this;
-    };
-    chart.hover = function (fin, fout) {
-        !dots && createDots();
-        dots.mouseover(fin).mouseout(fout);
-        return this;
-    };
-    chart.click = function (f) {
-        !dots && createDots();
-        dots.click(f);
-        return this;
-    };
-    chart.each = function (f) {
-        createDots(f);
-        return this;
-    };
-    chart.eachColumn = function (f) {
-        createColumns(f);
-        return this;
-    };
-    return chart;
-};
+    this.path(paths.topShadow).attr({
+      'fill': colors.remaining,
+      'stroke': 'none',
+      'opacity': colors.shadowOpacity
+    });
+    this.path(paths.bottomShadow).attr({
+      'fill': colors.added,
+      'stroke': 'none',
+      'opacity': colors.shadowOpacity
+    });
+    this.path(paths.top).attr({
+      'stroke': colors.remaining,
+      'stroke-width': '2px'
+    });
+    this.path(paths.bottom).attr({
+      'stroke': colors.added,
+      'stroke-width': '2px'
+    });
 
+    
+    // Draw optimal burn down line
+    this.path(new Path()
+      .move(data[0].coords.center, data[0].coords.top)
+      .line(corners.right - .5 * columnWidth, baseOriginal + accAdded * axis.deltaY)
+    ).attr({
+      'stroke': colors.optimal
+    }).toBack();
+
+  };
+  
 })();
