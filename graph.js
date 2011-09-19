0951ea38ad3819fdb749434501eac0945c3b3700
Glenn Nilsson <glenn.nilsson@gmail.com>
2011-09-19 10:55:48 +0200

    Data

--- graph.js
+++ graph.js
@@ -1,8 +1,8 @@
 var data = [
-  { label: '2011-09-13', remaining: 146, added: 0 },
+  { label: '2011-09-13', remaining: 146 },
   { label: '2011-09-14', remaining: 125, added: 5 },
   { label: '2011-09-15', remaining: 110, added: 1 },
-  { label: '2011-09-16' },
+  { label: '2011-09-16', remaining: 101 },
   { label: '2011-09-19' },
   { label: '2011-09-20' },
   { label: '2011-09-21' },
