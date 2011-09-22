185899ced3d0b27e2eebc8f74785d4609d3c0a75
Glenn Nilsson <glenn.nilsson@gmail.com>
2011-09-22 10:44:18 +0200

    New data again.

--- graph.js
+++ graph.js
@@ -5,7 +5,7 @@ var data = [
   { label: '2011-09-16', remaining: 101 },
   { label: '2011-09-19', remaining: 94, added: 3 },
   { label: '2011-09-20', remaining: 83, added: 3 },
-  { label: '2011-09-21' },
+  { label: '2011-09-21', remaining: 79 },
   { label: '2011-09-22' },
   { label: '2011-09-23' }
 ]
